025c9dd880fdf096cfef357225db2787f882864e
nictheriscer <nicolas.riscalas@stu.ocsb.ca>
2022-03-01 03:26:01 +0000

    hfdsj

---
+++ area_per_circle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created By: Nicolas Riscalas
+
+# Date: 02-28-2022
+
+# Calculates the area and perimeter of a square
+
+import math
+
+PI = math.pi
+
+
+def main():
+    r = float(input("Enter the radius of a circle:"))
+    perimeter = PI * r * 2
+    area = PI * r * r
+    print("Area of a circle = %.2f" % area)
+    print("Perimeter of a circle = %.2f" % perimeter)
+
+
+if __name__ == "__main__":
+    main()
